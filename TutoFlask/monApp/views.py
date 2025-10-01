95dddbaccd8fc8c8ead0f5d8f2a3491032d54093
ClémentM <clement.moisan@etu.univ-orleans.fr>
2025-10-01 14:04:44 +0200

    commit du TP 4

--- TutoFlask/monApp/views.py
+++ TutoFlask/monApp/views.py
@@ -1,11 +1,17 @@
 from .app import app
-from flask import render_template
+from flask import render_template, request
+from monApp.models import Auteur
+from monApp.models import Livre
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="Cricri")
-
+    # si pas de paramètres
+    if len(request.args)==0:
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name="Cricri")
+    else :
+        param_name = request.args.get('name')
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name=param_name)
 
 @app.route('/contact')
 def contact():
@@ -14,6 +20,17 @@ def contact():
 @app.route('/about')
 def about():
     return render_template("about.html",title ="R3.01 Dev Web avec Flask",name="Arsouze")
+
+@app.route('/auteurs/')
+def getAuteurs():
+    lesAuteurs = Auteur.query.all()
+    return render_template('auteurs_list.html', title="R3.01 Dev Web avec Flask", auteurs=lesAuteurs)
+
+@app.route('/livres/')
+def getlivres():
+    lesLivres = Livre.query.all()
+    print(Livre.query.count())
+    return render_template('livre_list.html', title="R3.01 Dev Web avec Flask", livres=lesLivres)
 
 if __name__ == "__main__":
     app.run()
