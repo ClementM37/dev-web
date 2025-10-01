dc6740b3c6b4ebf7406071fd6d3feeb37c61f86f
ClémentM <clement.moisan@etu.univ-orleans.fr>
2025-10-01 15:33:20 +0200

    mid TP5

--- TutoFlask/config.py
+++ TutoFlask/config.py
@@ -3,3 +3,4 @@ SECRET_KEY = "2lzUl{$*D6#`8uXqlU."
 ABOUT = "Bienvenue sur la page à propos de Flask !"
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'monApp.db')
+BOOTSTRAP_SERVE_LOCAL = True
--- TutoFlask/monApp/app.py
+++ TutoFlask/monApp/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_bootstrap5 import Bootstrap
+
 app = Flask ( __name__ )
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
@@ -8,3 +10,6 @@ app.config.from_object('config')
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 db.init_app(app)
+
+# Bootstrap
+Bootstrap(app)
